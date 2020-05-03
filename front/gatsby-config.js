065ecdcf5b0a4474226b7faa455d7ba18fa63446
kaznak <kaznak.at.work@gmail.com>
2020-05-03 20:05:18 +0900

    chor(front): setup gatsby-plugin-graphql-codegen

--- front/gatsby-config.js
+++ front/gatsby-config.js
@@ -6,5 +6,13 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-typescript`],
+  plugins: [
+    `gatsby-plugin-typescript`,
+    {
+      resolve: "gatsby-plugin-graphql-codegen",
+      options: {
+        fileName: `types/graphql-types.d.ts`,
+      },
+    },
+  ],
 }
